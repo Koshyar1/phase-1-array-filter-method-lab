3fb3984538246f1cc74264d3e25fccda7c7ca961
koshyar <ensw03420@student.su.edu.krd>
2024-03-16 13:58:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(list, name) {
+  return list.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(list, name) {
+  return list.filter((driver) => driver.startsWith(name));
+}
+
+function matchName(list, name) {
+  return list.filter((driver) => driver.name === name);
+}
